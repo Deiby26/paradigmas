6fb2612d8af85a87e472e71342c8643c7ba73e69
David Pulgarin <davidbohorquez150@gmail.com>
2023-09-27 21:11:37 -0500

    imagen api

--- my-app/app.vue
+++ my-app/app.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>
-    <h1>Hola Mundo!</h1>
-    <h2>Hola Mundo!</h2>
-    <h3>Hola Mundo°</h3>
-  </div>
+  <v-app>
+    <div>
+      <v-container v-for="personaje in personajes" :key="personaje.id">
+        <v-card>
+          
+          <v-img
+            :src="
+              personaje.thumbnail.path + '.' + personaje.thumbnail.extension
+            " 
+          >
+          </v-img>
+          
+
+        </v-card>
+      </v-container>
+    </div>
+  </v-app>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'IndexPage',
+
+  data() {
+    return {
+      personajes: [],
+    }
+  },
+  created() {
+    const url =
+      'http://gateway.marvel.com/v1/public/characters?ts=8&apikey=&hash='
+    
+    axios
+      .get(url)
+      .then((response) => (this.personajes = response.data.data.results))
+  },
+}
+</script>
+
+ <style>
+  p{margin-top: -20px; margin-left: 16px;}
+</style>
